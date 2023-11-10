10ee72ba86c47f71267899040e9e780d4589eb88
mhenrique94 <mhenrique94@outlook.com>
2023-11-10 20:00:12 -0300

    adiciona vuetify

--- src/main.js
+++ src/main.js
@@ -3,9 +3,23 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi'
+  },
+})
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router).use(vuetify)
 
 app.mount('#app')
